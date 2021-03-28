1b695f6e41dbb41431eab905514994ae3a7518d3
NathanOliveiraJF <58376382+NathanOliveiraJF@users.noreply.github.com>
2021-03-28 04:40:23 -0300

    add pages and update layouts

---
+++ Buffet/Controllers/AdminController.cs
@@ -0,0 +1,47 @@
+﻿using Buffet.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buffet.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public AdminController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Help()
+        {
+            return View();
+        }
+
+        public IActionResult PanelUser()
+        {
+            return View();
+        }
+
+        public IActionResult Session()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
