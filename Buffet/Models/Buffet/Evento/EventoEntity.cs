083266bebc4173a81febf78b6e171edb94598279
NathanOliveiraJF <58376382+NathanOliveiraJF@users.noreply.github.com>
2021-04-25 11:53:54 -0300

    add LocalEntity and EventoEntity

--- Buffet/Models/Buffet/Evento/EventoEntity.cs
+++ Buffet/Models/Buffet/Evento/EventoEntity.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Buffet.Models.Buffet.Evento;
+using Buffet.Models.Buffet.Local;
+using Buffet.Models.Buffet.Situacao;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +11,16 @@ namespace Buffet.Models.Buffet
     public class EventoEntity
     {
         public Guid Id { get; set; }
+        public Guid ClienteId { get; set; }
+        public SituacaoEventoEntity Situacao { get; set; }
+        public TipoEventoEntity Tipo { get; set; }
+        public LocalEntity Local { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataInicio { get; set; }
+        public DateTime DataTermino { get; set; }
+        public DateTime DataInserido { get; set; }
+        public DateTime DataModificacao { get; set; }
+        public string TextoObservacao { get; set; }
         public string Nome { get; set; }
         public ClienteEntity Cliente { get; set; }
         public EventoEntity()
---
+++ Buffet/Models/Buffet/Local/LocalEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buffet.Models.Buffet.Local
+{
+    public class LocalEntity
+    {
+        public string Descricao { get; set; }
+        public string Endereco { get; set; }
+    }
+}
