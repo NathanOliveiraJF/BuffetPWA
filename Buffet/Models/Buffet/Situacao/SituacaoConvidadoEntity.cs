2210cbd3912fb21b5f3ddd5184a618f3cfd21523
NathanOliveiraJF <58376382+NathanOliveiraJF@users.noreply.github.com>
2021-04-25 11:37:02 -0300

    add class Situacao

---
+++ Buffet/Models/Buffet/Situacao/SituacaoConvidadoEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buffet.Models.Buffet.Situacao
+{
+    public class SituacaoConvidadoEntity
+    {
+        public SituacaoConvidadoEntity(Guid id, string descricao)
+        {
+            Id = id;
+            Descricao = descricao;
+        }
+
+        public Guid Id { get; private set; }
+        public string Descricao { get; set; }
+    }
+}
---
+++ Buffet/Models/Buffet/Situacao/SituacaoEventoEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buffet.Models.Buffet.Situacao
+{
+    public class SituacaoEventoEntity
+    {
+        public SituacaoEventoEntity(Guid id, string descricao)
+        {
+            Id = id;
+            Descricao = descricao;
+        }
+
+        public Guid Id { get; private set; }
+        public string Descricao { get; set; }
+    }
+}
