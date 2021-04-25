0112110dcdde1a3359710b80cbc34c0aa2ba50d3
NathanOliveiraJF <58376382+NathanOliveiraJF@users.noreply.github.com>
2021-04-25 11:47:33 -0300

    add class ClienteEntity

--- Buffet/Models/Buffet/Cliente/ClienteEntity.cs
+++ Buffet/Models/Buffet/Cliente/ClienteEntity.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Buffet.Models.Buffet.Cliente;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,14 +8,22 @@ namespace Buffet.Models.Buffet
 {
     public class ClienteEntity
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; private set; }
+        public TipoClienteEntity Tipo { get; set; }
         public string Nome { get; set; }
         public string Email { get; set; }
-        public ICollection<EventoEntity> Eventos { get; set; }
+        public string Endereco { get; set; }
+        public string Cpf { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Cnpj { get; set; }
+        public string TextoObservacao { get; set; }
+        public DateTime DataInserido { get; set; }
+        public DateTime DataModificacao { get; set; }
+    
         public ClienteEntity()
         {
             Id = new Guid();
-            Eventos = new List<EventoEntity>();
+    
         }
     }
 }
