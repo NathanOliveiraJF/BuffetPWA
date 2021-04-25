14454d42cdf6e9afa93cf6206daef0727e9f8aff
NathanOliveiraJF <58376382+NathanOliveiraJF@users.noreply.github.com>
2021-04-25 11:32:54 -0300

    add class Tipo

---
+++ Buffet/Models/Buffet/Cliente/TipoClienteEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buffet.Models.Buffet.Cliente
+{
+    public class TipoClienteEntity
+    {
+        public TipoClienteEntity(Guid id, string descricao)
+        {
+            Id = id;
+            Descricao = descricao;
+        }
+
+        public Guid Id { get; private set; }
+        public string Descricao { get; set; }
+    }
+}
---
+++ Buffet/Models/Buffet/Evento/TipoEventoEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buffet.Models.Buffet.Evento
+{
+    public class TipoEventoEntity
+    {
+        public TipoEventoEntity(Guid id, string descricao)
+        {
+            Id = id;
+            Descricao = descricao;
+        }
+
+        public Guid Id { get; private set; }
+        public string Descricao { get; set; }
+    }
+}
